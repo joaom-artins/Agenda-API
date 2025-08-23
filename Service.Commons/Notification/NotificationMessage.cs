fdce5b924bd39e4d0d523e978519ebeb17bb9b98
joaom-artins <milanezijoao51@gmail.com>
2025-08-23 13:52:12 -0300

    cria filtro de fluentValidation

--- Service.Commons/Notification/NotificationMessage.cs
+++ Service.Commons/Notification/NotificationMessage.cs
@@ -2,4 +2,11 @@
 
 public class NotificationMessage
 {
+    public static class Common
+    {
+        public static readonly string UnexpectedError = "Erro inesperado!";
+        public static readonly string ValidationError = "Ocorreram um ou mais erros de validação!";
+        public static readonly string RequestListRequired = "Lista da requisição não pode estar vazia!";
+        public static readonly string DataExists = "Dados já cadastrados";
+    }
 }
