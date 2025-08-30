32cb9ca311b55b30a84312e7d10f8ca2155f44e1
joaom-artins <milanezijoao51@gmail.com>
2025-08-29 22:05:59 -0300

    Cria repositories

---
+++ Service.Data/Repositories/Interfaces/v1/IAvailabilitySlotRepository.cs
@@ -0,0 +1,8 @@
+﻿using Service.Data.Repositories.Generic.Interfaces;
+using Service.Domain.Models.v1;
+
+namespace Service.Data.Repositories.Interfaces.v1;
+
+public interface IAvailabilitySlotRepository : IGenericRepository<AvailabilitySlotModel>
+{
+}
---
+++ Service.Data/Repositories/v1/AvailabilitySlotRepository.cs
@@ -0,0 +1,13 @@
+﻿using Service.Data.Context;
+using Service.Data.Repositories.Generic;
+using Service.Data.Repositories.Interfaces.v1;
+using Service.Domain.Models.v1;
+
+namespace Service.Data.Repositories.v1;
+
+public class AvailabilitySlotRepository(
+    AppDbContext context
+) : GenericRepository<AvailabilitySlotModel>(context),
+    IAvailabilitySlotRepository
+{
+}
